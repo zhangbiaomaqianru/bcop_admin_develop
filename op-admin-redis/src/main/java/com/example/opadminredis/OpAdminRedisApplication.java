3924b2d5d083c5638b617f0758942bddc189651b
zhangbiao <zhangbiao@cmss.chinamobile.com>
2020-08-28 14:30:08 +0800

    op-admin-redis工程生成
    Code Source From: Self Code
    # Self Code / OpenSource / 3rd party Code / Other
    Description:

---
+++ op-admin-redis/src/main/java/com/example/opadminredis/OpAdminRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.opadminredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpAdminRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpAdminRedisApplication.class, args);
+    }
+
+}
---
+++ op-admin-redis/src/test/java/com/example/opadminredis/OpAdminRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opadminredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpAdminRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
